4f2529836ecc8420884ff108de13bf1f73a532f5
Vylune <126494170+Vylune@users.noreply.github.com>
2023-03-13 17:21:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ def generate_grid(size):
         grid.append(l)
     return grid
 
+def gravite(grille):
+    for i in range(len(grille)-1):
+        for j in range(len(grille[i])):
+            if grille[i+1][j]==0:
+                grille[i+1][j]=grille[i][j]
+                grille[i][j]=0
+    return grille
